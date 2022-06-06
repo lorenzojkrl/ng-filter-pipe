485d8c5f845907924751d2e2ee2ae4f27c946e14
Lorenzo <lorenzozar@gmail.com>
2022-06-06 17:01:09 +0300

    add users feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -240,4 +240,30 @@ export class AppComponent {
       },
     },
   ];
+
+  onAddUser() {
+    this.usersList.push({
+      id: Math.floor(Math.random() * 10000),
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      address: {
+        street: 'Kulas Light',
+        suite: 'Apt. 556',
+        city: 'Gwenborough',
+        zipcode: '92998-3874',
+        geo: {
+          lat: '-37.3159',
+          lng: '81.1496',
+        },
+      },
+      phone: '1-770-736-8031 x56442',
+      website: 'hildegard.org',
+      company: {
+        name: 'Romaguera-Crona',
+        catchPhrase: 'Multi-layered client-server neural-net',
+        bs: 'harness real-time e-markets',
+      },
+    });
+  }
 }
